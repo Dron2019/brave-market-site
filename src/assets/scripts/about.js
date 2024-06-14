0e4add64c088e04736ff0ff13535b31907b9ad55
Volodymyr Isyk <volodka.isyk.95@gmail.com>
2024-06-14 10:18:58 +0300

    refactor: added gsap animation on accordion in about page

--- src/assets/scripts/about.js
+++ src/assets/scripts/about.js
@@ -1,124 +1,66 @@
 import Swiper from 'swiper/bundle';
-import { collapseTextChangeRangesAcrossMultipleVersions } from 'typescript';
 import sideSwitchArrow from './modules/side-switch-arrow';
 import splitToLinesAndFadeUp from './modules/effects/splitLinesAndFadeUp';
-// import 'swiper/swiper.scss';
-
-const questionList = document.querySelector('.about-screen5__list.about-list');
-
-const breakpointsForScreenSlide3 = {
-  320: {
-    slidesPerGroup: 1,
-    slidesPerView: 'auto',
-    spaceBetween: 8,
-  },
-  600: {
-    slidesPerGroup: 1,
-    slidesPerView: 1.3,
-    spaceBetween: 16,
-  },
-  800: {
-    slidesPerGroup: 1,
-    slidesPerView: 2.3,
-    spaceBetween: 20,
-  },
-  1100: {
-    slidesPerGroup: 2,
-    slidesPerView: 3,
-    spaceBetween: 20,
-  },
-  1200: {
-    spaceBetween: 20,
-    slidesPerView: 4,
-  },
-};
-const breakpointsForScreenSlider4 = {
-  350: {
-    slidesPerView: 1,
-    spaceBetween: 5,
-  },
-  480: {
-    slidesPerGroup: 1,
-    slidesPerView: 1,
-    spaceBetween: 8,
-  },
-  600: {
-    slidesPerView: 1.2,
-    spaceBetween: 10,
-  },
-  800: {
-    slidesPerView: 1.7,
-    spaceBetween: 10,
-  },
-  1024: {
-    slidesPerView: 2.3,
-    spaceBetween: 20,
-  },
-};
-
-const initSLider = (
-  sliderSelector,
-  nextBtnSelector,
-  prevBtnSelector,
-  adaptObj,
-  paginationSelector = '',
-  simulateTouch = false,
-) => {
-  const swiper = new Swiper(sliderSelector, {
-    loop: true,
-    autoplay: true,
-    preloadImages: false,
-    simulateTouch: !simulateTouch,
-    lazy: true,
-    speed: 800,
-    watchSlidesVisibility: true,
-    navigation: {
-      nextEl: document.querySelector(nextBtnSelector),
-      prevEl: document.querySelector(prevBtnSelector),
-    },
-    pagination: {
-      el: paginationSelector,
-      type: 'fraction',
-      formatFractionCurrent: num => {
-        return num > 9 ? num : '0' + num;
-      },
-      formatFractionTotal: num => {
-        return num > 9 ? num : '0' + num;
-      },
-      renderFraction: function(currentClass, totalClass) {
-        return (
-          '<span class="' +
-          currentClass +
-          '">' +
-          '</span>' +
-          '/' +
-          '<span class="' +
-          totalClass +
-          '">' +
-          '</span>'
-        );
-      },
-    },
-    breakpoints: adaptObj,
-  });
-
-  return swiper;
-};
-
-questionList.addEventListener('click', evt => {
-  const button = evt.target.closest('[data-about-item-label]');
-
-  if (!button) return;
-  if (button.closest('.answer-active')) {
-    button.classList.remove('answer-active');
-    button.nextElementSibling.style.display = 'none';
-  } else {
-    button.classList.add('answer-active');
-    button.nextElementSibling.style.display = 'block';
-  }
-});
+import { gsap } from 'gsap/all';
 
 document.addEventListener('DOMContentLoaded', () => {
+  splitToLinesAndFadeUp('.text-style-1920-h-1');
+
+  document.querySelectorAll('.about-item').forEach(item => {
+    createTimeline(item);
+    attachEvents(item);
+  });
+
+  const breakpointsForScreenSlide3 = {
+    320: {
+      slidesPerGroup: 1,
+      slidesPerView: 'auto',
+      spaceBetween: 8,
+    },
+    600: {
+      slidesPerGroup: 1,
+      slidesPerView: 1.3,
+      spaceBetween: 16,
+    },
+    800: {
+      slidesPerGroup: 1,
+      slidesPerView: 2.3,
+      spaceBetween: 20,
+    },
+    1100: {
+      slidesPerGroup: 2,
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    1200: {
+      spaceBetween: 20,
+      slidesPerView: 4,
+    },
+  };
+  const breakpointsForScreenSlider4 = {
+    350: {
+      slidesPerView: 1,
+      spaceBetween: 5,
+    },
+    480: {
+      slidesPerGroup: 1,
+      slidesPerView: 1,
+      spaceBetween: 8,
+    },
+    600: {
+      slidesPerView: 1.2,
+      spaceBetween: 10,
+    },
+    800: {
+      slidesPerView: 1.7,
+      spaceBetween: 10,
+    },
+    1024: {
+      slidesPerView: 2.3,
+      spaceBetween: 20,
+    },
+  };
+
   let prevButton;
   let nextButton;
 
@@ -172,8 +114,102 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 });
 
+function initSLider(
+  sliderSelector,
+  nextBtnSelector,
+  prevBtnSelector,
+  adaptObj,
+  paginationSelector = '',
+  simulateTouch = false,
+) {
+  const swiper = new Swiper(sliderSelector, {
+    loop: true,
+    autoplay: true,
+    preloadImages: false,
+    simulateTouch: !simulateTouch,
+    lazy: true,
+    speed: 800,
+    watchSlidesVisibility: true,
+    navigation: {
+      nextEl: document.querySelector(nextBtnSelector),
+      prevEl: document.querySelector(prevBtnSelector),
+    },
+    pagination: {
+      el: paginationSelector,
+      type: 'fraction',
+      formatFractionCurrent: num => {
+        return num > 9 ? num : '0' + num;
+      },
+      formatFractionTotal: num => {
+        return num > 9 ? num : '0' + num;
+      },
+      renderFraction: function(currentClass, totalClass) {
+        return (
+          '<span class="' +
+          currentClass +
+          '">' +
+          '</span>' +
+          '/' +
+          '<span class="' +
+          totalClass +
+          '">' +
+          '</span>'
+        );
+      },
+    },
+    breakpoints: adaptObj,
+  });
+
+  return swiper;
+}
+
 function pad(num) {
   return num < 10 ? '0' + num : num;
 }
 
-splitToLinesAndFadeUp('.text-style-1920-h-1');
+function createTimeline(accordion) {
+  const timeline = gsap.timeline({
+    paused: true,
+    onStart: timelineStart,
+    onStartParams: [accordion],
+    onReverseCompleteParams: [accordion],
+  });
+
+  timeline
+    .fromTo(
+      accordion.querySelector('[data-about-item-answer]'),
+      {
+        height: 0,
+        marginTop: 0,
+      },
+      {
+        marginTop: 20,
+        height: 'auto',
+        duration: 0.8,
+      },
+    )
+    .fromTo(
+      accordion.querySelector('[data-about-item-close-icon]'),
+      { rotate: 0 },
+      { rotate: -45 },
+      '<',
+    );
+
+  timeline.reverse();
+
+  accordion.querySelector('[data-about-item-label]').accordionTimeline = timeline;
+}
+
+function timelineStart(accordion) {
+  accordion.querySelector('[data-about-item-answer]').style.display = 'block';
+}
+
+function attachEvents(accordion) {
+  const trigger = accordion.querySelector('[data-about-item-label]');
+
+  trigger.addEventListener('click', () => {
+    trigger.accordionTimeline.reversed()
+      ? trigger.accordionTimeline.play()
+      : trigger.accordionTimeline.reverse();
+  });
+}
